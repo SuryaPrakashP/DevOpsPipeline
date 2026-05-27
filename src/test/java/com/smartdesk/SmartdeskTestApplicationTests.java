52e65de6b4dee77790bc3f9ac2b013c0cdcf6688
Surya Prakash <suryaprakash3537@gmail.com>
2026-05-27 17:05:26 +0530

    first commit

---
+++ src/test/java/com/smartdesk/SmartdeskTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartdeskTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
